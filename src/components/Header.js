5e2a45b679c9db6a672c5176803fe048118362ec
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-11-21 23:17:19 -0500

    add logo and background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header =(props) =>{
 
     return (<nav>
         <Link to="/signup"><h2>Signup</h2></Link>
-        <Link to="/Login"><h2>Login</h2></Link>
+        <Link to="/login"><h2>Login</h2></Link>
         {gState.token ? logout : null }
     </nav>)
 }
